86ac92d056f9b052d7477145cb95b7a87eaa6f19
Minh Vu <vqminh@users.noreply.github.com>
2018-06-29 09:12:36 +0700

    Update download_brw.bat

--- Samples/C#/CalculatorTest/download_brw.bat
+++ Samples/C#/CalculatorTest/download_brw.bat
@@ -1,3 +1,9 @@
 call cleanup.bat
 gsutil cp gs://lender-rate.appspot.com/%1/BRW/* C:\Users\ADMIN\Documents\input\
-for /r %%x in (*) do move "%%x" "%%x.brw"
+setlocal EnableDelayedExpansion
+set filename=A
+set suffix=1
+for /F "delims=" %%i in ('dir') do (
+   set /A suffix+=1
+   ren "%%i" "%filename%-!suffix:~1!.brw"
+)
