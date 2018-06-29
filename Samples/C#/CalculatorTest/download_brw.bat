cec6a6b9495e77bf68588809d5a70dfa6df64e40
Minh Vu <vqminh@users.noreply.github.com>
2018-06-29 10:23:48 +0700

    Update download_brw.bat

--- Samples/C#/CalculatorTest/download_brw.bat
+++ Samples/C#/CalculatorTest/download_brw.bat
@@ -1,3 +1,3 @@
 call cleanup.bat
-call gsutil cp gs://lender-rate.appspot.com/%1/BRW/* C:\Users\ADMIN\Documents\input\
+call gsutil mv gs://lender-rate.appspot.com/%1/BRW/* C:\Users\ADMIN\Documents\input\
 call rename.bat brw
