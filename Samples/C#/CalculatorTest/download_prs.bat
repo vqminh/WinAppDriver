d3df78bd25c9786baf74bdbaf05c6e6f2780e131
Minh Vu <vqminh@users.noreply.github.com>
2018-06-29 11:50:48 +0700

    Update download_prs.bat

--- Samples/C#/CalculatorTest/download_prs.bat
+++ Samples/C#/CalculatorTest/download_prs.bat
@@ -1,3 +1,3 @@
 call cleanup.bat
-call gsutil mv gs://lender-rate.appspot.com/%1/PRS/* C:\Users\ADMIN\Documents\input\
+call gsutil -m mv gs://lender-rate.appspot.com/%1/PRS/* C:\Users\ADMIN\Documents\input\
 call rename.bat prs
